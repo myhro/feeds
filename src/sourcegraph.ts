87a1b76f989a0fd6027c13d7175b4f85b18dc84a
Tiago Ilieve <tiago.myhro@gmail.com>
2022-11-04 07:19:19 -0300

    sourcegraph: fix link assembling
    'path.join()' ends up removing one slash from the 'https://' prefix.

--- src/sourcegraph.ts
+++ src/sourcegraph.ts
@@ -1,10 +1,8 @@
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
-import path from 'path';
 import { Feed } from 'feed';
 
 (async () => {
-  const root = 'https://about.sourcegraph.com';
   const url = 'https://about.sourcegraph.com/blog';
   const feed = new Feed({
     id: url,
@@ -12,13 +10,14 @@ import { Feed } from 'feed';
     copyright: '',
   });
 
+  const root = 'https://about.sourcegraph.com';
   const data = await fetch(url);
   const $ = cheerio.load(await data.text());
   const posts = $('.card').slice(0, 10);
   for (const p of posts) {
     const title = $('.tw-block', p).text();
     const href = $('.tw-block', p).attr('href') || '';
-    const link = path.join(root, href);
+    const link = new URL(href, root).href;
     const time = $('time', p).text();
     const date = new Date(time);
     const description = $('.row p', p).text();
